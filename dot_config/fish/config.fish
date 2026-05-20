eb9270e948080d66b1d4b1440899c3f5e4cc4e5a
Floris Cornel <65155757+floriscornel@users.noreply.github.com>
2026-05-20 12:03:25 +0200

    fix: fish loading mise , restructure mise/config.toml

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,13 @@
-if test -f ~/.local/share/mise/bin/mise
+# Load mise
+if test -f ~/.local/bin/mise
+    ~/.local/bin/mise activate fish | source
+else if test -f ~/.local/share/mise/bin/mise
     ~/.local/share/mise/bin/mise activate fish | source
 end
+
+# Load starship
 if test -f ~/.config/starship.toml
     starship init fish | source
 end
+
+export PATH="/opt/homebrew/bin:$PATH"
