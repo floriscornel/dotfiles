580d8ea6edf2ade9edaea6b217da29ac2cad5a95
Floris Cornel <65155757+floriscornel@users.noreply.github.com>
2026-05-25 14:35:27 +0200

    add zerostack and fix 01

--- run_once_01_install_mise.sh
+++ run_once_01_install_mise.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 echo "Installing mise..."
-curl https://mise.run | sh
+if ! command -v mise &> /dev/null; then
+    curl https://mise.run | sh
+else
+    echo "mise is already installed, skipping."
+fi
 
 echo "Installing global tools via mise..."
 # Ensure the path is temporarily available for the script
 export PATH="$HOME/.local/share/mise/bin:$PATH"
 
 # Setup GitHub authentication to avoid rate limits during installation
-mise x gh -- gh auth login
+# if `mise x gh -- gh auth status` output does not contain  `Active account: true` then run `mise x gh -- gh auth login` to authenticate with GitHub
+if ! mise x gh -- gh auth status 2>&1 | grep -q "Active account: true"; then
+  mise x gh -- gh auth login
+fi
 
 mise install -y
