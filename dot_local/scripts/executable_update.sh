47a834f75b94ef4a14466bc5a7d042a9c29b9cf3
Floris Cornel <65155757+floriscornel@users.noreply.github.com>
2026-06-10 12:05:38 +0200

    run pitchfork supservisor after upgrade

--- dot_local/scripts/executable_update.sh
+++ dot_local/scripts/executable_update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 CURRENT_DIR=$(pwd)
 EXECUTION_DIR=~/
-cd $EXECUTION_DIR && mise self-update && mise upgrade -i && cd $CURRENT_DIR
+cd $EXECUTION_DIR \
+    && mise self-update \
+    && mise upgrade -i \
+    && pitchfork supervisor start --force \
+&& cd $CURRENT_DIR
